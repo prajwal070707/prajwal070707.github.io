eb65f578fd8e4541a95256f29d99b3305c6ef70a
Prajwal <shresthaprajwal@yahoo.com>
2021-07-15 20:30:32 -0500

    React initialized

---
+++ prajwal.website/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        This is react
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >Hello world
+        </a>
+      
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
